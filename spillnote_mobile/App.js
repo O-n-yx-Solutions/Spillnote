5dee2a784d807cb9ce3021fb94f47dc6ffb38299
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-11-01 13:45:39 -0600

    Updated Account page mobile

--- spillnote_mobile/App.js
+++ spillnote_mobile/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RegisterPage from './Components/regPageMobile';
+import LoginPage from './Components/loginPageMobile';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginPage />
     </View>
   );
 }
