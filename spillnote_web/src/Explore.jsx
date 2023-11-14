84fe6b5318c428fd8ba00b026ba2f7e10773576b
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2023-11-13 22:20:40 -0700

    Fix console warning for class/className

--- spillnote_web/src/Explore.jsx
+++ spillnote_web/src/Explore.jsx
@@ -1,29 +1,22 @@
-import './App.css';
-
-
-
+import "./App.css";
 
 const Explore = () => {
-
-    return (
-        <div class="gallery-container">
-            <div class="gallery" id="gallery">
-                <div class="gallery-item">1</div>
-                <div class="gallery-item">2</div>
-                <div class="gallery-item">3</div>
-                <div class="gallery-item">4</div>
-                <div class="gallery-item">5</div>
-                <div class="gallery-item">6</div>
-                <div class="gallery-item">7</div>
-                <div class="gallery-item">8</div>
-                <div class="gallery-item">9</div>
-            </div>
-            <button id="addGalleryItem">Add Gallery Item</button>
-        </div>
-
-    );
+  return (
+    <div className="gallery-container">
+      <div className="gallery" id="gallery">
+        <div className="gallery-item">1</div>
+        <div className="gallery-item">2</div>
+        <div className="gallery-item">3</div>
+        <div className="gallery-item">4</div>
+        <div className="gallery-item">5</div>
+        <div className="gallery-item">6</div>
+        <div className="gallery-item">7</div>
+        <div className="gallery-item">8</div>
+        <div className="gallery-item">9</div>
+      </div>
+      <button id="addGalleryItem">Add Gallery Item</button>
+    </div>
+  );
 };
 
 export default Explore;
-
-
--- spillnote_web/src/util.js
+++ spillnote_web/src/util.js
@@ -1,42 +1,53 @@
-import { collection, addDoc, setDoc, doc, deleteDoc, query, where, getDocs, serverTimestamp, updateDoc } from 'firebase/firestore';
-import getFirestore from './Firebase';
+import {
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+  deleteDoc,
+  query,
+  where,
+  getDocs,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+//import getFirestore from "./Firebase";
 
-export const handleNew = async() => {
-    const value = prompt("Enter a note");
+export const handleNew = async () => {
+  const value = prompt("Enter a note");
 
-    const collectionRef = collection(getFirestore, "notes");
-    const payload = { class: value, timestamp: serverTimestamp() };
+  const collectionRef = collection(getFirestore, "notes");
+  const payload = { className: value, timestamp: serverTimestamp() };
 
-    await addDoc(collectionRef, payload);
-}
-
-export const handleEdit = async(id) => {
-    const value = prompt("Edit note");
-
-    const docRef = doc(getFirestore, "notes", id);
-    const payload = { class: value, timestamp: serverTimestamp() };
-
-    await updateDoc(docRef, payload);
+  await addDoc(collectionRef, payload);
 };
 
-export const handleDelete = async(id) => {
-    const docRef = doc(getFirestore, "notes", id);
+export const handleEdit = async (id) => {
+  const value = prompt("Edit note");
+
+  const docRef = doc(getFirestore, "notes", id);
+  const payload = { className: value, timestamp: serverTimestamp() };
+
+  await updateDoc(docRef, payload);
+};
+
+export const handleDelete = async (id) => {
+  const docRef = doc(getFirestore, "notes", id);
+  await deleteDoc(docRef);
+};
+
+export const handleQueryDelete = async () => {
+  const userQInput = prompt("Enter a note");
+
+  const collectionRef = collection(getFirestore, "notes");
+  const q = query(collectionRef, where("className", "==", userQInput));
+  const snapshot = await getDocs(q);
+
+  const results = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+  console.log(results);
+
+  results.forEach(async (result) => {
+    const docRef = doc(getFirestore, "notes", result.id);
     await deleteDoc(docRef);
-};
-
-export const handleQueryDelete = async() => {
-    const userQInput = prompt("Enter a note");
-
-    const collectionRef = collection(getFirestore, "notes");
-    const q = query(collectionRef, where("class", "==", userQInput));
-    const snapshot = await getDocs(q);
-
-    const results = snapshot.docs.map((doc) => ({...doc.data(), id: doc.id }));
-
-    console.log(results);
-
-    results.forEach(async result => {
-        const docRef = doc(getFirestore, "notes", result.id);
-        await deleteDoc(docRef);
-    })
+  });
 };
