8e66c845fc1cdbd8d908070b974fc3f0ba4c6985
Zane Dayton <Day16006@byui.edu>
2023-11-29 18:10:13 -0700

    fixed the file extension

--- spillnote_web/src/Components/settingsPage.jsx
+++ spillnote_web/src/Components/settingsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Sun from "../../public/settingsIcons/Sun"
-import Moon from "../../public/settingsIcons/Moon"
+import Sun from "../../public/settingsIcons/Sun.svg?react"
+import Moon from "../../public/settingsIcons/Moon.svg?react"
 // import Sun from "./images/Sun.svg?react";
 // import Moon  from "./images/Moon.svg?react";
 import "./styles/DarkMode.css";
