44379a38a5fc2fc39b4d46b83570f2cf7f53152d
Zane Dayton <Day16006@byui.edu>
2023-12-07 02:48:12 -0700

    maybe setting will work

--- spillnote_web/src/Components/settingsPage.jsx
+++ spillnote_web/src/Components/settingsPage.jsx
@@ -9,16 +9,16 @@ const SettingsPage = () => {
         const setDarkMode = () => 
         {
             document.querySelector("body").setAttribute('data-theme', 'dark')
-        }
+        };
         const setLightMode = () => 
         {
             document.querySelector("body").setAttribute('data-theme', 'light')
-        }
+        };
         const toggleTheme = e =>
         {
             if(e.target.checked) setDarkMode();
             else setLightMode();
-        }
+        };
     return (
         <div className='dark_mode'>
             {/* <Header /> */}
