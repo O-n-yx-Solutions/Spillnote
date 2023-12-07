02811ec9c921a807dcea7d891d18deaab132129b
Zane Dayton <Day16006@byui.edu>
2023-12-06 20:02:55 -0700

    updated explore branch

--- spillnote_mobile/App.js
+++ spillnote_mobile/App.js
@@ -9,31 +9,12 @@ import Explore from "./Components/Explore";
 import LoginPage from "./Components/loginPageMobile";
 import RegisterPage from "./Components/regPageMobile";
 
-<<<<<<< HEAD
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginPage />
-      <StatusBar style="auto" />
-      <Explore />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-=======
 const navigator = createStackNavigator(
   {
     Home: Nav,
     Explore: Explore,
     LoginPage: LoginPage,
     RegisterPage: RegisterPage,
->>>>>>> 56ca9f2cc4c6fea3bf52031c6b00dbc956866eec
   },
   {
     initialRouteName: "Home",
