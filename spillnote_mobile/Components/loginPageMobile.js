c21e305ae6f70a79f3fd0c03a22749165a09bfca
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-12-11 15:59:25 -0700

    changes to Login page

--- spillnote_mobile/Components/loginPageMobile.js
+++ spillnote_mobile/Components/loginPageMobile.js
@@ -1,34 +1,103 @@
 import React from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { login } from '../firebase';
+import { useState } from 'react';
 
-function LoginPage() {
+function LoginPage({navigation}) {
+  const handleNavigation = (screen) => 
+  {
+    navigation.navigate(screen);
+  };
+
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+
+  const handleChange = (event) => 
+  {
+    switch (event.target.name) {
+      case "username":
+        setUsername(event.target.value);
+        break;
+      case "password":
+        setAge(event.target.value);
+        break;
+    }
+  };
+
     return (
         <View>
-        <Text>Log In</Text>
+        <Text style={styles.heading}>Log In</Text>
         <View>
-          <Text>Username:</Text>
+          <Text style={styles.text}>Username:</Text>
           <TextInput
+            name="username"
+            onChange={handleChange}
             placeholder="Enter your username"
-            style={{ borderWidth: 1, borderColor: 'black', borderRadius: 5 }}
+            style={styles.inputboxes}
           />
         </View>
-  
         <View>
-          <Text>Password:</Text>
+          <Text style={styles.text}>Password:</Text>
           <TextInput
+            name="password"
             placeholder="Enter your password"
             secureTextEntry={true}
-            style={{ borderWidth: 1, borderColor: 'black', borderRadius: 5 }}
+            style={styles.inputboxes}
           />
         </View>
   
-        <Button title="Log In" onPress={() => console.log('Log In clicked')} />
+        {/* <Button title="Log In" onPress={() => console.log('Log In clicked')} /> */}
   
-        <TouchableOpacity onPress={() => console.log('Register clicked')}>
-          <Text>Register</Text>
+        <TouchableOpacity style={styles.login} onPress={(username) => console.log(username.value)}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+        <TouchableOpacity style={styles.reg} onPress={() => handleNavigation("RegisterPage")}>
+          <Text style={styles.buttonText}>Need an Account? Register</Text>
         </TouchableOpacity>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    login: {
+      backgroundColor: "#3f4966",
+      margin: 5,
+      padding: 8,
+    },
+    reg: {
+      backgroundColor: "#3f4966",
+      margin: 5,
+      padding: 8,
+    },
+    buttonText: {
+      color: "#ffffff",
+      fontSize: 16,
+      textAlign: "center",
+    },
+    heading:
+    {
+      textAlign: 'center',
+      fontSize: 20,
+    },
+    inputboxes:
+    {
+      margin: 5,
+      borderWidth: 1, 
+      borderColor: 'black', 
+      borderRadius: 5
+    },
+    text:
+    {
+      marginLeft: 5,
+    },
+
+    image: {
+      width: 200,
+      height: 200,
+      resizeMode: "cover",
+    },
+  });
   
   export default LoginPage;
--- spillnote_mobile/firebase.js
+++ spillnote_mobile/firebase.js
@@ -25,4 +25,84 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 // console.log(db)
+
+export function signup(email, password) {
+    return auth().createUserWithEmailAndPassword(email, password);
+  }
+  
+  export function login(email, password) {
+    return auth().signInWithEmailAndPassword(email, password);
+  }
+  
+  export function logout() {
+    return auth().signOut();
+  }
+  
+  // Custom Hook
+  export function useAuth() {
+    const [currentUser, setCurrentUser] = useState();
+  
+    useEffect(() => {
+      const unsub = auth().onAuthStateChanged((user) => setCurrentUser(user));
+      return unsub;
+    }, []);
+  
+    return currentUser;
+  }
+  
+
+
 export default db;
+
+// import { initializeFirestore, initializeApp } from 'firebase/firestore';
+// import { useEffect, useState } from 'react';
+// import auth from '@react-native-firebase/auth';
+// import firestore from '@react-native-firebase/firestore';
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//     databaseURL: "https://spillnote-f2023-default-rtdb.firebaseio.com",
+//     projectId: "spillnote-f2023",
+//     storageBucket: "spillnote-f2023.appspot.com",
+//     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//     appId: process.env.REACT_APP_APPID
+// };
+
+// // Initialize Firebase
+// // if (!auth.apps.length) {
+//   auth.initializeApp(firebaseConfig);
+// // }
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const db = getFirestore(app);
+// // console.log(db)
+// // export default db;
+
+// // const db = firestore();
+
+// export function signup(email, password) {
+//   return auth().createUserWithEmailAndPassword(email, password);
+// }
+
+// export function login(email, password) {
+//   return auth().signInWithEmailAndPassword(email, password);
+// }
+
+// export function logout() {
+//   return auth().signOut();
+// }
+
+// // Custom Hook
+// export function useAuth() {
+//   const [currentUser, setCurrentUser] = useState();
+
+//   useEffect(() => {
+//     const unsub = auth().onAuthStateChanged((user) => setCurrentUser(user));
+//     return unsub;
+//   }, []);
+
+//   return currentUser;
+// }
+
+// export default db;
