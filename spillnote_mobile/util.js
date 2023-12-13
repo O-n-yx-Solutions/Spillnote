0f536a5d3e58d17ab4738408c76629594e7cccaa
KabutoDrip <cammiller42003@gmail.com>
2023-12-13 15:40:48 -0500

    handleQuery

--- spillnote_mobile/util.js
+++ spillnote_mobile/util.js
@@ -24,47 +24,35 @@ import {
         timestamp: serverTimestamp(),
         deltaContent: deltaContent
       };
-  
       const docRef = await addDoc(collectionRef, payload);
+
       console.log("Document written with ID: ", docRef.id);
     } catch (error) {
       console.error("Error adding document: ", error);
     }
   };
-
-export const handleEdit = async(id) => {
-    const value = prompt("Edit note");
-
-    const docRef = doc(db, "notes", id);
-    const payload = { class: value, timestamp: serverTimestamp() };
-
-    await updateDoc(docRef, payload);
-};
+  //creates a new note 
 
 export const editDbText = async (id,text) => {
     console.log(`ID: ${id}, Text: ${text}`)
 
-    const docRef = doc(db, "notes", id); // leave same
-   
+    const docRef = doc(db, "notes", id); 
     const payload = { content: text, timestamp: serverTimestamp() };
   
     updateDoc(docRef, payload);
   };
-
+//edits the contents of a note
   
   export const fetchNotes = async (userEmail) => {
     try {
       if (userEmail) {
         const q = query(collection(db, "notes"), where("email", "==", userEmail));
         const querySnapshot = await getDocs(q);
-  
         const notes = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
-        }));
-  
-        // console.log("Fetched Notes:", notes); // Log all the results
-  
+        }));  
+
         return querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
@@ -78,25 +66,5 @@ export const editDbText = async (id,text) => {
       throw error;
     }
   };
+//gets all notes that belong to a specific user
 
-export const handleDelete = async(id) => {
-    const docRef = doc(db, "notes", id);
-    await deleteDoc(docRef);
-};
-
-export const handleQueryDelete = async() => {
-    const userQInput = prompt("Enter a note");
-
-    const collectionRef = collection(db, "notes");
-    const q = query(collectionRef, where("class", "==", userQInput));
-    const snapshot = await getDocs(q);
-
-    const results = snapshot.docs.map((doc) => ({...doc.data(), id: doc.id }));
-
-    console.log(results);
-
-    results.forEach(async result => {
-        const docRef = doc(db, "notes", result.id);
-        await deleteDoc(docRef);
-    })
-};
